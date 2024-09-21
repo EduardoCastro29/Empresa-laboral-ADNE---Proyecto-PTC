4cc1c865cbd76f5f0b5256cce893b5f2c3f30e6b
sanguchitoou <2017razor2017@gmail.com>
2024-09-20 23:25:23 -0600

    XD

--- Modelo/DAO/DAOConfiguracionUsuario.cs
+++ Modelo/DAO/DAOConfiguracionUsuario.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Empresa_laboral_ADNE___Proyecto_PTC.Vista;
-using System.Data;
-using System.Data.SqlClient;
-using Empresa_laboral_ADNE___Proyecto_PTC.Controlador;
-using Empresa_laboral_ADNE___Proyecto_PTC.Modelo.DTO;
-using System.Windows.Forms;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Empresa_laboral_ADNE___Proyecto_PTC.Vista;
+//using System.Data;
+//using System.Data.SqlClient;
+//using Empresa_laboral_ADNE___Proyecto_PTC.Controlador;
+//using Empresa_laboral_ADNE___Proyecto_PTC.Modelo.DTO;
+//using System.Windows.Forms;
 
-namespace Empresa_laboral_ADNE___Proyecto_PTC.Modelo.DAO
-{
-    internal class DAOConfiguracionUsuario
-    {
-        readonly SqlCommand Conexion = new SqlCommand();
-        //Este es el método común dentro del apartado de configuración del usuario
-        public bool ActualizarUsuarioConfig()
-        {
-            try
-            {
-                //Inicializamos la conexión
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ha ocurrido un error, ERR-003-6", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-            finally
-            {
-                Conexion.Connection.Close();
-            }
-        }
-    }
-}
+//namespace Empresa_laboral_ADNE___Proyecto_PTC.Modelo.DAO
+//{
+//    internal class DAOConfiguracionUsuario
+//    {
+//        readonly SqlCommand Conexion = new SqlCommand();
+//        //Este es el método común dentro del apartado de configuración del usuario
+//        public bool ActualizarUsuarioConfig()
+//        {
+//            try
+//            {
+//                //Inicializamos la conexión
+//            }
+//            catch (Exception)
+//            {
+//                MessageBox.Show("Ha ocurrido un error, ERR-003-6", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//                return false;
+//            }
+//            finally
+//            {
+//                Conexion.Connection.Close();
+//            }
+//        }
+//    }
+//}
